fb4a38e9a11394a3f66d4e057bdbfaf49cbe7c47
JohanHerbert <johanherbert99@gmail.com>
2024-09-05 07:48:33 +0200

    Created file and folder for reading and storing data

---
+++ dataReading.py
@@ -0,0 +1,37 @@
+
+def readDataFromYahoo():
+    # Todo    
+    return 0
+
+def readDataFromFile():
+    # Todo
+    return 0
+
+def saveDataToCsv():
+    # Todo
+    return 0
+
+def toReturns():
+    # Todo
+    return 0
+
+def toLogReturns():
+    # Todo
+    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
