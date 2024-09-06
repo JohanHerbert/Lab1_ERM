48820fe780aaf314d7ad7c5c2acfa4d9e8307887
JohanHerbert <johanherbert99@gmail.com>
2024-09-06 16:04:34 +0200

    Added functions to download, save and load data from yahoo

--- dataReading.py
+++ dataReading.py
@@ -1,25 +1,23 @@
 import yfinance as yf
+import pandas as pd
 
 def readDataFromYahoo(ticker, startDate, endDate):
     stocks = yf.Ticker(ticker)
     stockData = stocks.history(start = startDate , end = endDate)
     return stockData
 
-def readDataFromFile():
-    # Todo
-    return 0
+def readDataFromFile(fileName):
+    price = pd.read_csv('DataFileFolder\\' + fileName, index_col = 'Date' , parse_dates = True)
+    return price
 
-def saveDataToCsv(stockData, filename, stockName):
-    startDate = stockData.index.min()
-    endDate = stockData.index.max()    
-
-    stockData.to_csv('DataFileFolder\\' + 'price_'+ stockName + f'_{startDate}'+ f'_{endDate}'+'.csv', index=False)
-    # filepath = 'C:\\ ..\\'
-# price.to_csv(filepath + 'apple_price_data.csv')
+def saveDataToCsv(stockData, stockName):
+    # startDate = stockData.index.min()
+    # endDate = stockData.index.max()    
+    fileName = 'price_'+ stockName + '.csv'
+    directory = 'DataFileFolder\\'+ fileName
+    stockData.to_csv(directory)
+    return fileName
     
-    # Todo
-    return 0
-
 def toReturns():
     # Todo
     return 0
