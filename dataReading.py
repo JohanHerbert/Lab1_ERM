dbe0262ea306e30b832a7ea1fea244865a59baf8
JohanHerbert <johanherbert99@gmail.com>
2024-09-06 12:19:02 +0200

    Added data reading function

--- dataReading.py
+++ dataReading.py
@@ -1,13 +1,22 @@
+import yfinance as yf
 
-def readDataFromYahoo():
-    # Todo    
-    return 0
+def readDataFromYahoo(ticker, startDate, endDate):
+    stocks = yf.Ticker(ticker)
+    stockData = stocks.history(start = startDate , end = endDate)
+    return stockData
 
 def readDataFromFile():
     # Todo
     return 0
 
-def saveDataToCsv():
+def saveDataToCsv(stockData, filename, stockName):
+    startDate = stockData.index.min()
+    endDate = stockData.index.max()    
+
+    stockData.to_csv('DataFileFolder\\' + 'price_'+ stockName + f'_{startDate}'+ f'_{endDate}'+'.csv', index=False)
+    # filepath = 'C:\\ ..\\'
+# price.to_csv(filepath + 'apple_price_data.csv')
+    
     # Todo
     return 0
 
